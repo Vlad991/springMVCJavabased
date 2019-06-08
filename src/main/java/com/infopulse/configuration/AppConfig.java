6f74588ab01ee1e3b36efb236bc9e029c5ed997d
vlad <vladkuzma99@gmail.com>
2019-06-08 10:47:16 +0300

    my commit

--- src/main/java/com/infopulse/configuration/AppConfig.java
+++ src/main/java/com/infopulse/configuration/AppConfig.java
@@ -1,8 +1,8 @@
 package com.infopulse.configuration;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+        import org.springframework.context.annotation.ComponentScan;
+        import org.springframework.context.annotation.Configuration;
+        import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @Configuration
 @EnableWebMvc
